a5fee02ffa6d3db5fa572c4673c50df77ae6af28
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-06-18 19:54:03 +0200

    done

--- index.js
+++ index.js
@@ -1,70 +1,97 @@
-import express from "express"
-import * as dotenv from "dotenv"
+import * as dotenv from 'dotenv'
+import express, { request, response } from 'express'
 
-// Load .env file
+// Maak een nieuwe express app
+const app = express()
+
 dotenv.config()
 
-/* -------------------------------------------------------------------------- */
-/*                                Server setup                                */
-/* -------------------------------------------------------------------------- */
-const server = express()
+const url = "https://api.werktijden.nl/2/employees";
 
-server.set("view engine", "ejs")
-server.set("views", "./views")
-server.set("port", process.env.PORT || 8000)
-server.use(express.static('public'))
-
-server.listen(server.get("port"), () => {
-	console.log(`Application started on http://localhost:${server.get("port")}`)
-})
-
-const date = new Date()
-const start = new Date().toJSON().slice(0, 10)
-const end = new Date(date.setDate(date.getDate() + 1)).toJSON().slice(0, 10)
-
-
-/* -------------------------------------------------------------------------- */
-/*                                Server Routes                               */
-/* -------------------------------------------------------------------------- */
-server.get("/", async (req, res) => {
-	const employees = await dataFetch("https://api.werktijden.nl/2/employees")
-	const punches = await dataFetch(`https://api.werktijden.nl/2/timeclock/punches?departmentId=98756&start=${start}&end=${end}`)
-	//console.log(employees)
-	// console.log(punches)
-	res.render("index", {employees, punches, title:"Aanwezigheidsoverzicht"})
-})
-
-server.post("/", async (req, res) => {
-	const postData = {
-		"employee_id": 368786,
-		"department_id": 98756,
-	}
-
-	postJson("https://api.werktijden.nl/2/timeclock/clockin", postData).then((data) => {
-		if (data.status == 200) {
-			res.redirect("/inklokken")
-			console.log("Status 200: Done!")
-		}
-	})
-})
-
-server.get("/inklokken", async (req, res) => {
-	res.render("inklokken", {title:"Inklokken"})
-})
-
-server.get("/uitklokken", async (req, res) => {
-	res.render("uitklokken", {title:"Uitklokken"})
-})
-
-/* -------------------------------------------------------------------------- */
-/*                                API Functions                               */
-/* -------------------------------------------------------------------------- */
 const options = {
 	method: "GET",
 	headers: {
 		Authorization: `Bearer ${process.env.API_KEY}`,
 	}
 }
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'));
+
+// Stel afhandeling van formulieren in
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Maak een route voor de index
+app.get("/", (request, response) => {
+  console.log(request.query.employees);
+  dataFetch(url).then((data) => {
+    console.log(data)
+    response.render("index",{employee:data});
+  });
+});
+
+// inklok magic
+
+app.post("/inklokken", async (req, res) => {
+	const departmentId = Number(req.body.department)
+	const employeeId = Number(req.body.employee)
+
+	const postData = {
+		"employee_id": employeeId,
+		"department_id": departmentId,
+	}
+
+	postJson("https://api.werktijden.nl/2/timeclock/clockin", postData)
+	res.redirect("/")
+})
+
+app.get("/inklokken", async (req, res) => {
+	const departments = await dataFetch("https://api.werktijden.nl/2/departments")
+	const employees = await dataFetch("https://api.werktijden.nl/2/employees")
+  
+	res.render("inklokken", {title:"Inklokken", departments, employees})
+})
+
+app.post("/uitklokken", async (req, res) => {
+	const departmentId = Number(req.body.department)
+	const employeeId = Number(req.body.employee)
+
+	const postData = {
+		"employee_id": employeeId,
+		"department_id": departmentId,
+	}
+
+	postJson("https://api.werktijden.nl/2/timeclock/clockout", postData)
+	res.redirect("/")
+})
+
+app.get("/uitklokken", async (req, res) => {
+	const departments = await dataFetch("https://api.werktijden.nl/2/departments")
+	const employees = await dataFetch("https://api.werktijden.nl/2/employees")
+
+	res.render("uitklokken", {title:"Uitklokken", departments, employees})
+})
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
 async function dataFetch(url) {
 	const data = await fetch(url, options)
 		.then((response) => response.json())
@@ -75,10 +102,11 @@ async function dataFetch(url) {
 async function postJson(url, body) {
 	console.log(2, JSON.stringify(body));
 	return await fetch(url, {
-			method: "post",
+			method: "POST",
 			body: JSON.stringify(body),
 			headers: {
-				"Content-Type": "application/json"
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.API_KEY}`,
 			},
 		})
 		.then((response) => response.json())
